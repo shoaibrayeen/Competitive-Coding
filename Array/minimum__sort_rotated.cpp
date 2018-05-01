ca5161be0837756e466167db4fdf6c7e08af740a
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-05-01 21:09:55 +0530

    Minimum element in sorted and rotated array

--- Array/minimum__sort_rotated.cpp
+++ Array/minimum__sort_rotated.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int findMin(int arr[], int low, int high)
-{
+int findMin(int arr[], int low, int high) {
     // This condition is needed to handle the case when array is not
     // rotated at all
     if (high < low)  return arr[0];
@@ -28,10 +28,21 @@ int findMin(int arr[], int low, int high)
 }
 
 // Driver program to test above functions
-int main()
-{
+int main() {
+    int n;
+    cout << "\nEnter Number of Elemenets\t:\t";
+    cin >> n;
+    int * arr = new int[n];
+    cout << "\nEnter Element in Sorted and Rotated order\n";
+    for(int i = 0; i < n; ++i ) {
+        cin >> arr[i];
+    }
+    cout << "\nMinimum Element\t:\t" << findMin(arr , 0 , n-1);
+    delete[] arr;
+    /*
     int arr1[] =  {5, 6 ,7 , 10 , 1, 2, 3, 4};
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
     printf("The minimum element is %d\n", findMin(arr1, 0, n1-1));
+    */
     return 0;
 }
