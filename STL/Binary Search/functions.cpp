387cf5cb66b46d700f7145f7d059ffcc22a3f308
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-24 23:02:14 +0530

    updated binary search functions

--- STL/Binary Search/functions.cpp
+++ STL/Binary Search/functions.cpp
@@ -37,13 +37,13 @@ int main() {
     cout << "\nArray\t\t:\t";
     show(a, asize);                             // Output -> 10 , 20 , 20 , 30 , 40  , 50
     if ( binary_search(a, a + 6, 20) ) {         // searching 2 in the array
-        cout << "\nFound at\t:\t "<< (lower_bound(a , a + 5, 2) - a);  // Output -> 0
+        cout << "\nFound at\t:\t "<< (lower_bound(a , a + 5, 20) - a);  // Output -> 1
     }
     else {
         cout << "\n2 not found";          // print if function returns False
     }
     if ( binary_search(a, a + 6, 9) ) {         // Searching 9 in the array
-        cout << "\nFound at\t:\t "<< (lower_bound(a , a + 5, 2) - a);  // print if function returns True
+        cout << "\nFound at\t:\t "<< (lower_bound(a , a + 5, 9) - a);  // print if function returns True
     }
     else {
         cout << "\n9 not found\n";        // print if function returns False
