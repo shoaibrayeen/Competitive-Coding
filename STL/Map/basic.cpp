fc75a7c2f034d1dabf24ae189fce8f1d41e32246
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-26 10:01:04 +0530

    about Map

--- STL/Map/basic.cpp
+++ STL/Map/basic.cpp
@@ -20,3 +20,20 @@ int main() {
   cout << endl;
   return 0;
 }
+/*
+  Functions associated with Map:
+  begin() – Returns an iterator to the first element in the map
+  end() – Returns an iterator to the theoretical element that follows last element in the map
+  size() – Returns the number of elements in the map
+  max_size() – Returns the maximum number of elements that the map can hold
+  empty() – Returns whether the map is empty
+  pair insert(keyvalue,mapvalue) – Adds a new element to the map
+  erase(iterator position) – Removes the element at the position pointed by the iterator
+  erase(const g)- Removes the key value ‘g’ from the map
+  clear() – Removes all the elements from the map
+  key_comp() / value_comp() – Returns the object that determines how the elements in the map are ordered (‘<' by default)
+  find(const g) – Returns an iterator to the element with key value ‘g’ in the map if found, else returns the iterator to end
+  count(const g) – Returns the number of matches to element with key value ‘g’ in the map
+  lower_bound(const g) – Returns an iterator to the first element that is equivalent to mapped value with key value ‘g’ or definitely will not go before the element with key value ‘g’ in the map
+  upper_bound(const g) – Returns an iterator to the first element that is equivalent to mapped value with key value ‘g’ or definitely will go after the element with key value ‘g’ in the map
+*/
