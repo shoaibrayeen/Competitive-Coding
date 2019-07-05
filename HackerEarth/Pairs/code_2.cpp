62b4149264d822a51882bba652094b5e177d2564
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-07-06 01:28:37 +0530

    using preprocessing

---
+++ HackerEarth/Pairs/code_2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+bool prime(int n) {
+    int p = sqrt(n);
+    for(int i = 2; i <= p; i++) {
+        if(n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+	 int sieve[1000009];
+    sieve[0]=0, sieve[1]=0;
+    for(long int i=2;i<1000009;i++)
+        sieve[i]=1;
+        
+    for(long long int i=2;i*i<1000009;i++)
+    {
+        for(long long int j=i;j*i<1000009;j++)
+        {
+            sieve[i*j]=0;
+        }
+    }
+    
+    int pre_sum[1000009];
+    for(int i=0;i<1000009;i++)
+        pre_sum[i]=sieve[i];
+    
+    for(int i=1;i<1000009;i++)
+        pre_sum[i]=pre_sum[i]+pre_sum[i-1];
+    
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int l,r;
+        scanf("%d %d",&l,&r);
+        int prime=pre_sum[r]-pre_sum[l-1];
+        int comp=r-l+1-prime;
+        if(l==1)
+            comp-=1;
+        long int ans=prime*comp;
+        printf("%ld\n",ans);
+    }
+}
