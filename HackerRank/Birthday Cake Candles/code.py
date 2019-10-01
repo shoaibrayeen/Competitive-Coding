a3b8f27417d191cf57daf7878c9dc2e731643c47
Swapnil Madaan <31646351+SwapnilMadaan@users.noreply.github.com>
2019-10-01 13:54:05 +0530

    Updated Code with Local Machine Depenedency

--- HackerRank/Birthday Cake Candles/code.py
+++ HackerRank/Birthday Cake Candles/code.py
@@ -6,28 +6,20 @@ import random
 import re
 import sys
 
-# Complete the birthdayCakeCandles function below.
+# Complete the birthday Cake Candles function below.
 def birthdayCakeCandles(ar):
     max = ar[0]
     count = 0
-    for i in range(1,len(ar)):
+    for i in range(1, len(ar)):
         if max < ar[i]:
             max  = ar[i]
-    for i in range(0,len(ar)):
+    for i in range(0, len(ar)):
         if max == ar[i]:
             count = count + 1
     return count
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
+    ar = [ 4, 4, 1, 3]
     result = birthdayCakeCandles(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print( 'result\t:\t' , result)
 
