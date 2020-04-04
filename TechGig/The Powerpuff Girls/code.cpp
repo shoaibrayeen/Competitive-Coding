da821ef949ca8b98f3803d856197f64eafff4e8c
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-04-04 18:41:07 +0530

    Optimized Code in CPP

---
+++ TechGig/The Powerpuff Girls/code.cpp
@@ -0,0 +1,33 @@
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+long long int helper(vector<long> quantity, int n) {
+	long long int result = INT_MAX;
+	long long int labQuantity;
+	for( int i = 0; i < n; i++ ) {
+		cin >> labQuantity;
+		if ( quantity[i] != 0 ) {
+			labQuantity = int(labQuantity/quantity[i]);
+			if ( result > labQuantity ) {
+				result = labQuantity;
+			}
+		}
+	}
+	return result;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int n;
+    cin >> n;
+	vector<long> quantity(n);
+	for( int i = 0; i < n; i++ ) {
+		cin >> quantity[i];
+	}
+	
+	cout << helper(quantity , n) << "\n"; 
+}
